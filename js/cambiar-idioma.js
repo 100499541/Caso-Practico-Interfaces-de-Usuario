8bf5f79374fa072b20f35fff2d83a7fed37e4f54
100500454 <100500454@alumnos.uc3m.es>
2025-12-14 14:04:03 +0100

    Comentarios a cambiar-idioma.js

--- js/cambiar-idioma.js
+++ js/cambiar-idioma.js
@@ -1,27 +1,42 @@
+// Este codigo se encarga de la traduccion del sitio web
+// Utilizamos un archivo JSON para almacenar las traducciones
+// Y obtenemos los elementos a traducir mediante data-attributes en el html correspondiente
+
+// Primero, seleccionamos el selector de idioma y los textos a cambiar
 const languageSelect = document.querySelector("[data-language-selector]");
 const textsToChange = document.querySelectorAll("[data-section]");
 
-languageSelect.addEventListener("change", () => {
+// Esperamos a que el usuario cambie el idioma mediante el selector
+languageSelect.addEventListener("change", () => {ç
+    // Obtenemos la opcion seleccionada
     const selectedOption =
         languageSelect.options[languageSelect.selectedIndex];
 
+    // Guardamos la opcion en el localStorage para mantener la preferencia del usuario
     localStorage.setItem("language", selectedOption.dataset.language);
 
+    // Cargamos el archivo JSON correspondiente al idioma seleccionado
     fetch(`../idiomas/${selectedOption.dataset.language}.json`)
         .then(res => res.json())
         .then(data => {
             textsToChange.forEach((el) => {
+                // Obtenemos la seccion y el valor a traducir desde los data-attributes
                 const section = el.dataset.section;
                 const value = el.dataset.value;
-
+                
+                // Finalmente, cambiamos el texto del elemento por la traduccion correspondiente
                 el.innerHTML = data[section][value];
             })
         })
 })
 
+// Esta parte del codigo se encarga de mantener el idioma seleccionado al recargar la pagina
+
+// Al cargar la pagina, verificamos si hay un idioma guardado en el localStorage
 document.addEventListener("DOMContentLoaded", () => {
     const savedLanguage = localStorage.getItem("language");
-
+    // Si hay un idioma guardado, seleccionamos la opcion correspondiente en el selector
+    
     if (savedLanguage) {
         const option = languageSelect.querySelector(
             `option[data-language="${savedLanguage}"]`
