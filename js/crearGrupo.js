033fe0b5fa80814221e7cdbabd1d8e06f77b0ff5
100500454 <100500454@alumnos.uc3m.es>
2025-12-15 02:54:13 +0100

    mega commit

--- js/crearGrupo.js
+++ js/crearGrupo.js
@@ -1,19 +1,24 @@
+// Este código gestiona la apertura y cierre de un modal en el apartado social.
+
+// Primero seleccionamos los elementos necesarios del DOM.
 const openModal = document.querySelector(".hero__cta");
 const modal = document.querySelector(".modal");
 const closeModal = document.querySelector(".modal__close");
 
+// Añadimos los event listeners para abrir y cerrar el modal.
 openModal.addEventListener("click", (e) => {
     e.preventDefault();
     modal.classList.add("modal--show");
 });
 
+// Cerrar el modal al hacer clic en el botón de cerrar.
 closeModal.addEventListener("click", (e) => {
     e.preventDefault();
     modal.classList.remove("modal--show");
 });
 
+// Cerrar el modal al hacer clic fuera del contenido del modal.
 modal.addEventListener('click', (e) => {
-    // Si el click es directamente sobre el fondo (no el contenido)
     if (e.target === modal) {
         modal.classList.remove('modal--show');
     }
