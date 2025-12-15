d24252ea646f945012d1975b8dbf8d0d6f520fad
100500454 <100500454@alumnos.uc3m.es>
2025-12-15 01:39:37 +0100

    Creado una pequeña interfaz para crear grupo

---
+++ js/crearGrupo.js
@@ -0,0 +1,20 @@
+const openModal = document.querySelector(".hero__cta");
+const modal = document.querySelector(".modal");
+const closeModal = document.querySelector(".modal__close");
+
+openModal.addEventListener("click", (e) => {
+    e.preventDefault();
+    modal.classList.add("modal--show");
+});
+
+closeModal.addEventListener("click", (e) => {
+    e.preventDefault();
+    modal.classList.remove("modal--show");
+});
+
+modal.addEventListener('click', (e) => {
+    // Si el click es directamente sobre el fondo (no el contenido)
+    if (e.target === modal) {
+        modal.classList.remove('modal--show');
+    }
+});
